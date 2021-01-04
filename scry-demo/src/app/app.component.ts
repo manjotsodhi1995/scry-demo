4101c7d095b192d5bcce92338a0dbbb89a2bceaf
Manjot Singh <manjot.singh@tatadigital.com>
2021-01-04 15:36:55 +0530

    demo

--- scry-demo/src/app/app.component.ts
+++ scry-demo/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from "@angular/core";
+import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
+import { HttpClient } from "@angular/common/http";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'scry-demo';
+  title = "scry-demo";
+  loaded: boolean = false;
+  displayedColumns: string[] = [];
+  dataSource: MatTableDataSource<AnimationPlayState>;
+  symbol = "AAPL";
+  url = "https://financialmodelingprep.com/api/v3/quote/AAPL?apikey=demo";
+  financialStatement: any = [];
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+
+  constructor(private http: HttpClient) {
+    this.http.get<any>(this.url).subscribe(data => {
+      this.financialStatement = data;
+      this.createdDisplayComlumn(this.financialStatement[0]);
+
+      this.dataSource = new MatTableDataSource(this.financialStatement);
+      this.loaded = true;
+    });
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }, 1000);
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  createdDisplayComlumn(obj) {
+    Object.keys(obj).forEach((key, index) => {
+      this.displayedColumns.push(key);
+    });
+  }
 }
--- scry-demo/src/app/app.module.ts
+++ scry-demo/src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
+import { MatTableModule, MatPaginatorModule } from "@angular/material";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
